7676622676600633f1f5f7e814ef3cd112b052d2
Limnandi <limnandikent@gmail.com>
2025-11-11 22:50:32 +0200

    Removed dist build process and cleaned up compiled output files

--- dist/lib/auth-constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.encodedKey = void 0;
-var env_1 = require("@/lib/env");
-if (!env_1.env.NEXTAUTH_SECRET) {
-    throw new Error('NEXTAUTH_SECRET environment variable is required');
-}
-exports.encodedKey = new TextEncoder().encode(env_1.env.NEXTAUTH_SECRET);
